178951f8cd5597e9d55ca2885bd28e6f50e78aa5
Derik Nascimento <derikwnacimento@gmail.com>
2023-05-05 19:54:04 -0300

    pull

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-$(document).ready (function(){ 
+$(document).ready(function () {
     console.log((document.querySelector('header button')))
     console.log($('header button'))
+
+    $('header button').click(function () {
+        alert('Expandir formulario')
+    })
+    $('submit').on('submit', function (e) {
+        console.log('submit')
+        e.preventDefault()
+    })
 })
 
