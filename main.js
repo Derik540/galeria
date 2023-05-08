6ca9df08582fd74be24589e40979101c9e0cc107
Derik Nascimento <derikwnacimento@gmail.com>
2023-05-08 17:27:18 -0300

    FEATURE/ adicionado ação de mostrar e fechar formulário

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 $(document).ready(function () {
-    console.log((document.querySelector('header button')))
-    console.log($('header button'))
-
     $('header button').click(function () {
-        alert('Expandir formulario')
+        $('form').slideDown();
     })
+
+    $('blt-cancelar').click(function(){
+        $('form').slideUp();
+    })
+
     $('submit').on('submit', function (e) {
-        console.log('submit')
         e.preventDefault()
     })
 })
