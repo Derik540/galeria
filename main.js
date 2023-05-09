d7019e64a92e241db377f9e68faa9d099420296f
Derik Nascimento <derikwnacimento@gmail.com>
2023-05-09 18:38:54 -0300

    last

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
 $(document).ready(function () {
     $('header button').click(function () {
         $('form').slideDown();
-    })
+    });
 
-    $('blt-cancelar').click(function(){
+    $('#blt-cancelar').click(function () {
         $('form').slideUp();
-    })
+    });
 
-    $('submit').on('submit', function (e) {
-        e.preventDefault()
-    })
-})
+    $('form').submit(function (e) {
+        e.preventDefault();
+        const enderecoDaNovaImagem = $('#endereco-imagem').val();
+        const novoItem = $('<li style="display: none"></li>');
+        $(`<img src='${enderecoDaNovaImagem}'/>`).appendTo(novoItem);
+        $(`
+            <div class="overlay-image-link">
+                <a href="${enderecoDaNovaImagem}" target="_blank" title="ver imagem em tamanho real">
+                    ver tamanho real
+                </a>
+            </div>
+        `).appendTo(novoItem);
+        $('ul').append(novoItem);
+        $(novoItem).fadeIn();
+        $('#endereco-imagem').val('')
+    });
+});
 
